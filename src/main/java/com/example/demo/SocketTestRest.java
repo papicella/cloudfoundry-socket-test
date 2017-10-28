fe14d99cb1a53fdf3aec36a392060fae25c6760a
papicella <pas.apicella@tpg.com.au>
2017-10-28 20:36:17 +1100

    Added run instructions

--- src/main/java/com/example/demo/SocketTestRest.java
+++ src/main/java/com/example/demo/SocketTestRest.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,8 @@ import java.net.Socket;
 public class SocketTestRest
 {
 
+    protected static Logger logger = Logger.getLogger(SocketTestRest.class);
+
     @RequestMapping ("/{host}/{port}")
     public Result testConnection
             (@PathVariable String host, @PathVariable String port)
@@ -20,6 +23,9 @@ public class SocketTestRest
 
         try
         {
+            logger.info("Starting HTTP socket test ...");
+            logger.info(String.format("Attempting to connect to host %s on port %s", host, port));
+
             InetAddress inetAddress = InetAddress.getByName(host);
             s = new Socket(inetAddress, Integer.parseInt(port));
 
@@ -27,13 +33,15 @@ public class SocketTestRest
         }
         catch (Exception e)
         {
+            logger.info(String.format("Failed to connect to host %s on port %s", host, port));
             result = new Result("FAILED", e.getMessage(), host, port);
         }
         finally
         {
             if(s != null)
                 try {s.close();}
-                catch(Exception e){}
+                catch(Exception e) {
+                }
         }
 
         return result;
